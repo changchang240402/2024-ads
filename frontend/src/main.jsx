a7beec6d6afb80aed81a95ffb8ffa2bad7cbf84e
Tanh101 <lyvantanh101@gmail.com>
2024-02-16 13:34:43 +0700

    set up navigation with react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './index.css'
+import Login from './pages/Login/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path='' element={<Login/>}></Route>
+    </Routes>
+  </Router>
 )
