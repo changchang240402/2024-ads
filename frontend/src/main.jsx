1f87e51ff5925b0624cdc807c47492b5ac6ca609
Tanh101 <lyvantanh101@gmail.com>
2024-02-21 09:54:53 +0700

    create axios connect fe to be login function
    Please enter the commit message for your changes. Lines starting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
 import './index.css'
 import Login from './pages/Login/Login'
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path='' element={<Login/>}></Route>
     </Routes>
+    <ToastContainer />
   </Router>
 )
---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,36 @@
+import api from "../utility/api";
+import { useNavigate } from "react-router-dom";
+import { Toastify } from "../toastify/Toastify";
+
+function AuthService() {
+    const navigate = useNavigate();
+    const login = async (email, password) => {
+        try {
+            const response = await api.post("/auth/login", {
+                email,
+                password,
+            });
+            if (response.status === 200) {
+
+                Toastify.success("Login Successful");
+
+                localStorage.setItem("accessToken", response.data.access_token);
+                localStorage.setItem("refresponsehToken", response.data.refresh_token);
+                localStorage.setItem("userName", response.data.user.name);
+
+                const role = response.data.user.role;
+                role && navigate(`/${role === "admin" ? "admin" : "dashboad"}`);
+            }
+        } catch (error) {
+            if (error.response) {
+                Toastify.error(error.response.data.message);
+            }
+        }
+    };
+
+    return {
+        login,
+    };
+}
+
+export default AuthService;
