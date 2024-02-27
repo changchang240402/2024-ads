3223322e77c1f3d449c44d706c4cf967646ecd07
changchang240402 <thuytrangdao240402@gmail.com>
2024-02-27 15:33:42 +0700

    create campaign function

--- backend/app/Http/Controllers/CampaignController.php
+++ backend/app/Http/Controllers/CampaignController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Campaign\FilterCampaignRequest;
+use App\Http\Requests\Campaign\StoreCampaignRequest;
 use App\Models\Campaign;
 use App\Services\CampaignService;
 use Exception;
@@ -31,7 +32,6 @@ class CampaignController extends Controller
         $userId = auth()->id();
         $page = request()->get('page', 1);
         $validated = $request->validated();
-
         try {
             $campaign = $this->campaignService->filterCampaign($userId, $page, $validated);
         } catch (Exception $e) {
@@ -57,5 +57,30 @@ class CampaignController extends Controller
         }
 
         return response()->json($campaign, 200);
+    }
+
+    /**
+     * @param StoreCampaignRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function createCampaign(StoreCampaignRequest $request)
+    {
+        $validated = $request->validated();
+        $targetAudience = $validated['human'] . ' aged ' . $validated['start_age'] . '-' . $validated['end_age'] . ' ' . $validated['activities'] . ' activities';
+        try {
+            $validated['target_audience'] = $targetAudience;
+            $create = $this->campaignService->createCampaign($validated);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Create successfully!',
+            'data' => $create
+        ], 200);
     }
 }
--- backend/app/Http/Requests/Campaign/StoreCampaignRequest.php
+++ backend/app/Http/Requests/Campaign/StoreCampaignRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests\Campaign;
 
-use App\Rules\CustomUserRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreCampaignRequest extends FormRequest
 {
@@ -24,17 +26,27 @@ class StoreCampaignRequest extends FormRequest
     {
         return [
             'campaign_name' => 'required|string|max:255',
-            'user_id' => ['required', new CustomUserRule],
-            'campaign_goal'=> 'required|string|max:255',
+            'campaign_goal' => 'required|string|max:255',
             'budget' => 'required|numeric|between:0.00,99999999.99',
-            'start_date' => 'required|date_format:Y-m-d',
+            'start_date' => 'required|date_format:d-m-Y',
             'end_date' => [
                 'required',
-                'date_format:Y-m-d',
+                'date_format:d-m-Y',
                 'gte:start_date'
             ],
             'ad_message' => 'required|string|max:255',
-            'target_audience' => 'required|string|max:255',
+            'human' => [
+                'required',
+                Rule::in(config('constants.HUMAN_OBJECT'))
+            ],
+            'start_age' => 'required|integer|between:3,100',
+            'end_age' => [
+                'required',
+                'integer',
+                'between:3,100',
+                'gte:start_age'
+            ],
+            'activities' => 'required|string|max:100',
             'distribution_strategy' => 'required|string|max:255',
         ];
     }
@@ -43,6 +55,16 @@ class StoreCampaignRequest extends FormRequest
     {
         return [
             'end_date.gte' => 'End date must be some time after start date.',
+            'end_age.gte' => 'End date must be some time after start date.',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => $validator->errors()
+        ], 400));
     }
 }
