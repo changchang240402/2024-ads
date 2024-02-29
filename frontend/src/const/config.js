0331fd36c233567699bcfbb53d051dd0d48c39e6
Tanh101 <lyvantanh101@gmail.com>
2024-02-29 17:01:39 +0700

    connect api search, sort user in admin's dashboard

--- frontend/src/const/config.js
+++ frontend/src/const/config.js
@@ -3,3 +3,10 @@ export const FETCH_ADS_SUCCESS = 'FETCH_ADS_SUCCESS';
 export const ADS_PER_PAGE = 5;
 export const START_PAGE = 1;
 export const ADS_STATUS = ['Active', 'Paused'];
+export const USER_PER_PAGE = [5, 7];
+export const DEFAULT_USER_PER_PAGE = 5;
+export const USER_SORT_LABEL = 'USER_SORT_LABEL';
+export const USER_STATUS = {
+    active: 'active',
+    banned: 'banned'
+};
